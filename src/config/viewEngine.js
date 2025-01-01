4df12d3bb258e20882b16fd765f86a1714308f01
tqhbcken <tqh204152@gmail.com>
2025-01-01 21:54:24 +0700

    2

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -5,6 +5,7 @@ const viewEngine = (app) => {
   app.set("view engine", "ejs");
   app.set("views", "./src/views/");
 
+
   //khai bao static file
   app.use(express.static(path.join(__dirname, "../public")));
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,4 +5,17 @@ router.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
+
+router.get('/create-player', (req, res) => {
+    res.render('create-player.ejs');
+});
+
+router.get('/team', (req, res) => {
+    res.render('team.ejs');
+});
+
+router.post('/create-team', (req, res) => {
+    res.render('create-team.ejs');
+})
+
 module.exports = router;
